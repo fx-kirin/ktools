97c996f92dd9adb2a012b9dc5fdb897d08f105ab
Yoshiaki Ono <ono.kirin@gmail.com>
2018-10-09 10:05:29 +0900

    add categorical plot

--- ktools/__init__.py
+++ ktools/__init__.py
@@ -9,8 +9,7 @@ import time
 from bokeh.plotting import figure, output_file, show, ColumnDataSource
 from bokeh.models import HoverTool
 from bokeh.io import output_notebook
-
-__is_bokeh_loaded = False
+from bokeh.palettes import viridis
 
 def get_redundant_pairs(df):
     '''Get diagonal and lower triangular pairs of correlation matrix'''
@@ -40,11 +39,6 @@ def convert_datetimeindex_to_timestamp(index):
     return (index.astype(np.int64).astype(np.float) // 10**9) + time.timezone
 
 def bokeh_scatter(x, y):
-    global __is_bokeh_loaded
-    if not __is_bokeh_loaded:
-        output_notebook()
-        __is_bokeh_loaded = True
-
     source = ColumnDataSource(
             data=dict(
                 x=x,
@@ -64,4 +58,37 @@ def bokeh_scatter(x, y):
                title="Mouse over the dots")
 
     p.circle('x', 'y', size=5, source=source)
+    show(p)
+    
+def bokeh_categorical_scatter(df, x_label, y_label, category_label, desc=None):
+    hover = HoverTool(
+            tooltips=[
+                ("(x,y)", "($x, $y)"),
+                ("index", "@desc"),
+            ]
+        )
+    if desc is None:
+        desc = df[x_label].index
+
+    p = figure(plot_width=1600, plot_height=1000, tools=[hover, 'wheel_zoom', 'pan', 'box_zoom', 'reset'],
+               title="Mouse over the dots")
+
+    categories = df[category_label]
+    category_size = len(categories.unique())
+    colors = viridis(category_size)
+    name = df['業種'].name
+    for i, category in enumerate(categories.unique()):
+        p_x = df[df[name] == category][x_label]
+        p_y = df[df[name] == category][y_label]
+        p_desc = desc[df[name] == category]
+        source = ColumnDataSource(
+                data=dict(
+                    x=p_x,
+                    y=p_y,
+                    desc=p_desc,
+                )
+            )
+        p.circle('x', 'y', size=5, source=source, color=colors[i], legend=str(category))
+    p.legend.location = "top_right"
+    p.legend.click_policy="hide"
     show(p)
