815e185f65cc3eeb75ff161ca2516a710983ca31
fx_kirin <fx.kirin@gmail.com>
2020-11-12 19:09:36 +0900

    change db name

--- ktools/diskcache.py
+++ ktools/diskcache.py
@@ -1,7 +1,14 @@
+import traceback
 from pathlib import Path
 
-import diskcache
 import ring
+
+import diskcache
+
+trace = traceback.extract_stack()
+filename = Path(trace[-7].filename).name
+
+diskcache.core.DBNAME = f"{filename}.db"
 
 cache_dir = Path("~/.diskcache/ktool")
 cache_dir.mkdir(parents=True, exist_ok=True)
