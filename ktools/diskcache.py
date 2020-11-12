0badb47890f0ece4baf3d3c2a93daa9fdcf744ca
fx_kirin <fx.kirin@gmail.com>
2020-11-12 19:14:51 +0900

    done

--- ktools/diskcache.py
+++ ktools/diskcache.py
@@ -5,12 +5,6 @@ import ring
 
 import diskcache
 
-trace = traceback.extract_stack()
-filename = Path(trace[-7].filename).absolute()
-filename = str(filename).replace("/", "-")
-
-diskcache.core.DBNAME = f"{filename}.db"
-
 cache_dir = Path("~/.diskcache/ktool").expanduser().absolute()
 cache_dir.mkdir(parents=True, exist_ok=True)
 
